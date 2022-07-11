1caf85471cc3f7c13ddbe7aa018dd57be74b0f33
rsarona12021 <108763462+rsarona12021@users.noreply.github.com>
2022-07-11 18:56:39 -0500

    Update finalProject.js

--- Final project/finalProject.js
+++ Final project/finalProject.js
@@ -1,33 +1,40 @@
 
+
 let sonic
 let background1
+let rings
+
+
 
 let x = 100;
 let y = 100;
 let xPos;
 let yPos;
 
+
+
+
+
+
 function preload(){
 sonic=loadImage('sonic.png')
 background1=loadImage('background1.png')
-
-
-
-
-
-
-
+rings=loadImage('sonicrings.png')
 
 
 }
+
+
+
+
+
+
 
 
 function setup(){
 createCanvas(windowWidth,windowHeight)
-background(0)
+background(background1)
 
-xPos = random(windowWidth)
-yPos = random(windowHeight)
 
 
 
@@ -35,9 +42,19 @@ yPos = random(windowHeight)
 }
 
 
-
 function draw(){
 
+
+
+
+
+
+background(background1)
+
+
+for(let i= 0; i< 100; i++ ){
+  image(rings, random(windowWidth), random(windowHeight), 20 , 20)
+}
 
 
   if (keyIsDown(LEFT_ARROW)) {
@@ -60,15 +77,25 @@ function draw(){
     image(sonic, x, y, 50,50)
 
 
+    if (x < 0){
+       x +=10
+     }
+     if (x >= windowWidth){
+       x -= 50
+     }
+
+     if (y < 0){
+       y +=10
+     }
+     if (y >= windowHeight){
+       y -= 50
+     }
+
 
 
 
 
 }
-
-
-
-
 
 function windowResized(){
 
